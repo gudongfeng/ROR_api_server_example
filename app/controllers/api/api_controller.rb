a6652027a5dcff2ac57e4b640dd3a0de2105e783
Dongfeng gu <gudongfeng@outlook.com>
2017-10-09 10:56:31 -0700

    remove the useless file and fix the apipie localizetion problem

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,2 +1,11 @@
 class Api::ApiController < ApplicationController
+  # All other invalid API
+  api :GET, '/<invalid>', 'all other invalid api'
+  api :PUT, '/<invalid>', 'all other invalid api'
+  api :POST, '/<invalid>', 'all other invalid api'
+  api :DELETE, '/<invalid>', 'all other invalid api'
+  api_version 'root'
+  def invalid_api
+    render :json => I18n.t('not_found'), :status => :not_found
+  end
 end
